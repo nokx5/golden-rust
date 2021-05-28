882fb0a6921559d3c59bd997ef52a80e24970d77
nokx <72279573+nokx5@users.noreply.github.com>
2021-05-28 17:09:01 +0200

    add emacsExt, funny

--- shell.nix
+++ shell.nix
@@ -1,6 +1,25 @@
 { pkgs ? import <nixpkgs> { } }:
 with pkgs;
 let
+  emacsExt = let
+    emacsWithPackages =
+      (pkgs.emacsPackagesGen pkgs.emacs-nox).emacsWithPackages;
+  in emacsWithPackages (epkgs:
+    (with epkgs.melpaStablePackages; [
+      magit # ; Integrate git <C-x g>
+      zerodark-theme # ; Nicolas' theme
+    ]) ++ (with epkgs.melpaPackages;
+      [
+        # undohist
+        # zoom-frm # ; increase/decrease font size for all buffers %lt;C-x C-+>
+      ]) ++ (with epkgs.elpaPackages; [
+        undo-tree # ; <C-x u> to show the undo tree
+        # auctex # ; LaTeX mode
+        beacon # ; highlight my cursor when scrolling
+        nameless # ; hide current package name everywhere in elisp code
+      ]) ++ [
+        pkgs.notmuch # From main packages set
+      ]);
   vscodeExt = vscode-with-extensions.override {
     vscodeExtensions = with vscode-extensions;
       [ bbenoist.Nix eamodio.gitlens ]
@@ -11,12 +30,25 @@ let
           version = "0.31.0";
           sha256 = "McSWrOSYM3sMtZt48iStiUvfAXURGk16CHKfBHKj5Zk=";
         }
+        # {
+        #   name = "rust";
+        #   publisher = "rust-lang";
+        #   version = "0.7.8";
+        #   sha256 = "Y33agSNMVmaVCQdYd5mzwjiK5JTZTtzTkmSGTQrSNg0=";
+        # }
+        # {
+        #   name = "vscode-lldb";
+        #   publisher = "vadimcn";
+        #   version = "1.6.3";
+        #   sha256 = "5V7KMwv3/4C8N7wZCTq0RbQKuAshpWlLBHM3fRBc0PA=";
+        # }
         {
-          name = "rust";
-          publisher = "rust-lang";
-          version = "0.7.8";
-          sha256 = "Y33agSNMVmaVCQdYd5mzwjiK5JTZTtzTkmSGTQrSNg0=";
+          name = "vscode-lldb";
+          publisher = "vadimcn";
+          version = "1.6.3";
+          sha256 = "5V7KMwv3/4C8N7wZCTq0RbQKuAshpWlLBHM3fRBc0PA=";
         }
+
       ];
   };
 in mkShell {
@@ -27,7 +59,7 @@ in mkShell {
     gnumake
     nixfmt
     pkg-config
-    emacs-nox
+    emacsExt
     vscodeExt
   ] ++ [ typora ];
 
