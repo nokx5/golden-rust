30f515103c46d0e043903de2218f0ede030ef35a
nokx <72279573+nokx5@users.noreply.github.com>
2021-07-19 10:25:49 +0200

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -33,88 +33,87 @@
         golden-rust = with final; prev.callPackage ./derivation.nix {
           src = self;
         };
-        golden-rust-nightly = final.golden-rust; # not working yet
-        # golden-rust-nightly = prev.callPackage ./derivation.nix {
-        #   rustPlatform = prev.makeRustPlatform { inherit (final.rust-bin.stable.latest) rustc cargo; };
-        #   src = self;
-        # };
+        golden-rust-nightly = prev.callPackage ./derivation.nix {
+          rustPlatform = let rustSpecific = final.rust-bin.nightly.latest.minimal; in prev.makeRustPlatform { rustc = rustSpecific; cargo = rustSpecific; };
+          src = self;
+        };
       };
 
       devShell = forDevSystems (system:
         let pkgs = nixpkgsFor.${system}; in pkgs.callPackage ./shell.nix { }
       );
 
-      # hydraJobs = {
+      hydraJobs = {
 
-      #   build = forDevSystems (system: nixpkgsFor.${system}.golden-rust);
-      #   build-nightly = forDevSystems (system: nixpkgsFor.${system}.golden-rust-nightly);
+        build = forDevSystems (system: nixpkgsFor.${system}.golden-rust);
+        build-nightly = forDevSystems (system: nixpkgsFor.${system}.golden-rust-nightly);
 
-      #   docker = forDevSystems (system:
-      #     with nixpkgsFor.${system}; dockerTools.buildLayeredImage {
-      #       name = "${repoName}-docker-${repoVersion}";
-      #       tag = "latest";
-      #       created = "now";
-      #       contents = [ golden-rust ];
-      #       config = {
-      #         Cmd = [ "cli_golden" ];
-      #         # Env = [ "CMDLINE=ENABLED" ];
-      #         # ExposedPorts = { "8000" = { }; };
-      #       };
-      #     });
+        #   docker = forDevSystems (system:
+        #     with nixpkgsFor.${system}; dockerTools.buildLayeredImage {
+        #       name = "${repoName}-docker-${repoVersion}";
+        #       tag = "latest";
+        #       created = "now";
+        #       contents = [ golden-rust ];
+        #       config = {
+        #         Cmd = [ "cli_golden" ];
+        #         # Env = [ "CMDLINE=ENABLED" ];
+        #         # ExposedPorts = { "8000" = { }; };
+        #       };
+        #     });
 
-      #   # deb = forCustomSystems ["x86_64-linux"] (system: 
-      #   #   with nixpkgsFor.x86_64-linux; releaseTools.debBuild {
-      #   #   inherit stdenv;
-      #   #   name = "${repoName}-debian";
-      #   #   diskImage = vmTools.diskImageFuns.debian8x86_64 {};
-      #   #   src = golden-rust.src;
-      #   #   # buildInputs = [];
-      #   # });
+        #   # deb = forCustomSystems ["x86_64-linux"] (system: 
+        #   #   with nixpkgsFor.x86_64-linux; releaseTools.debBuild {
+        #   #   inherit stdenv;
+        #   #   name = "${repoName}-debian";
+        #   #   diskImage = vmTools.diskImageFuns.debian8x86_64 {};
+        #   #   src = golden-rust.src;
+        #   #   # buildInputs = [];
+        #   # });
 
-      #   # rpm = forCustomSystems ["x86_64-linux"] (system: 
-      #   #   with nixpkgsFor.x86_64-linux; releaseTools.rpmBuild {
-      #   #   name = "${repoName}-redhat";
-      #   #   diskImage = vmTools.diskImageFuns.centos7x86_64 {};
-      #   #   src = golden-rust.src;
-      #   #   # buildInputs = [];
-      #   # });
+        #   # rpm = forCustomSystems ["x86_64-linux"] (system: 
+        #   #   with nixpkgsFor.x86_64-linux; releaseTools.rpmBuild {
+        #   #   name = "${repoName}-redhat";
+        #   #   diskImage = vmTools.diskImageFuns.centos7x86_64 {};
+        #   #   src = golden-rust.src;
+        #   #   # buildInputs = [];
+        #   # });
 
-      #   # tarball =
-      #   #   nixpkgsFor.${system}.releaseTools.sourceTarball {
-      #   #     name = "${repoName} - tarball";
-      #   #     src = autoconf missing;
-      #   #   };
+        #   # tarball =
+        #   #   nixpkgsFor.${system}.releaseTools.sourceTarball {
+        #   #     name = "${repoName} - tarball";
+        #   #     src = autoconf missing;
+        #   #   };
 
-      #   # clang-analysis =
-      #   #   nixpkgsFor.${system}.releaseTools.clangAnalysis {
-      #   #     name = "${repoName}-nightly-analysis";
-      #   #     src = self;
-      #   #   };
+        #   # clang-analysis =
+        #   #   nixpkgsFor.${system}.releaseTools.clangAnalysis {
+        #   #     name = "${repoName}-nightly-analysis";
+        #   #     src = self;
+        #   #   };
 
-      #   # coverage =
-      #   #   nixpkgsFor.${system}.releaseTools.coverageAnalysis {
-      #   #     name = "${repoName}-coverage";
-      #   #     src = self.hydraJobs.tarball;
-      #   #     #lcovFilter = [ "*/tests/*" ];
-      #   #   };
+        #   # coverage =
+        #   #   nixpkgsFor.${system}.releaseTools.coverageAnalysis {
+        #   #     name = "${repoName}-coverage";
+        #   #     src = self.hydraJobs.tarball;
+        #   #     #lcovFilter = [ "*/tests/*" ];
+        #   #   };
 
-      #   release = forDevSystems (system:
-      #     with nixpkgsFor.${system}; releaseTools.aggregate
-      #       {
-      #         name = "${repoName}-release-${repoVersion}";
-      #         constituents =
-      #           [
-      #             self.hydraJobs.build.${system}
-      #             self.hydraJobs.build-nightly.${system}
-      #             #self.hydraJobs.docker.${system}
-      #           ] ++ lib.optionals (hostPlatform.isLinux) [
-      #             #self.hydraJobs.deb.x86_64-linux
-      #             #self.hydraJobs.rpm.x86_64-linux
-      #             #self.hydraJobs.coverage.x86_64-linux
-      #           ];
-      #         meta.description = "hydraJobs: ${repoDescription}";
-      #       });
-      # };
+        release = forDevSystems (system:
+          with nixpkgsFor.${system}; releaseTools.aggregate
+            {
+              name = "${repoName}-release-${repoVersion}";
+              constituents =
+                [
+                  self.hydraJobs.build.${system}
+                  self.hydraJobs.build-nightly.${system}
+                  #self.hydraJobs.docker.${system}
+                ] ++ lib.optionals (hostPlatform.isLinux) [
+                  #self.hydraJobs.deb.x86_64-linux
+                  #self.hydraJobs.rpm.x86_64-linux
+                  #self.hydraJobs.coverage.x86_64-linux
+                ];
+              meta.description = "hydraJobs: ${repoDescription}";
+            });
+      };
 
       packages = forAllSystems (system:
         {
@@ -138,13 +137,13 @@
 
       # defaultApp = forAllSystems (system: self.apps.${system}.golden-rust);
 
-      # templates = {
-      #   golden-rust = {
-      #     description = "template - ${repoDescription}";
-      #     path = ./.;
-      #   };
-      # };
+      templates = {
+        golden-rust = {
+          description = "template - ${repoDescription}";
+          path = ./.;
+        };
+      };
 
-      # defaultTemplate = self.templates.golden-rust;
+      defaultTemplate = self.templates.golden-rust;
     };
 }
